8bf053567784102917a97fa348a7b73d7cff150e
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-07-13 13:21:51 -0300

    Adiciona requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 
 // First commit
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const arrayReturned = [];
+  ids.forEach((id) => arrayReturned.push(species.find((specie) => specie.id === id)));
+  return arrayReturned;
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
